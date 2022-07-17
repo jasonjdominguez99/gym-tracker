f893d5fafa910f7355b2a90d1fe5714bb0fc1824
jasonjdominguez99 <jasonjdominguez99@users.noreply.github.com>
2022-07-17 15:15:26 +0100

    Created folders and boilerplate code for schema

---
+++ api/workout_template_exercises/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class WorkoutTemplateExercisesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'workout_template_exercises'
