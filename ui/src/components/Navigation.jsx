e82e3b6201d6be44e55be402d3d1fd23108a61e0
jasonjdominguez99 <jasonjdominguez99@users.noreply.github.com>
2022-07-18 22:32:21 +0100

    Added nav bar and footer components

---
+++ ui/src/components/Navigation.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+function Navigation() {
+  return (
+    <div className="navigation">
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <div className="container">
+          <NavLink className="navbar-brand" to="/">
+            Gym Tracker
+          </NavLink>
+          <div>
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/">
+                  Home
+                  <span className="sr-only">(current)</span>
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/workout-log">
+                  Workout Log
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/workout-plans">
+                  Workout Plans
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/my-account">
+                  My Account
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navigation;
