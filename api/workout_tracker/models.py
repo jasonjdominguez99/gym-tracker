602059dd5838f21581d87ac11d8e83b3388892e4
jasonjdominguez99 <jasonjdominguez99@users.noreply.github.com>
2022-07-18 12:32:20 +0100

    Edited models to make some attributes non-editable and/or not required

--- api/workout_tracker/models.py
+++ api/workout_tracker/models.py
@@ -17,7 +17,7 @@ class User(models.Model):
     last_name = models.CharField(max_length=35)
     email = models.EmailField()
     password = models.CharField(max_length=127)
-    sex = models.CharField(max_length=1, choices=sexes)
+    sex = models.CharField(max_length=1, choices=sexes, blank=True)
 
     def __str__(self):
         return self.first_name + self.last_name
@@ -36,8 +36,8 @@ class WorkoutLog(models.Model):
         on_delete=models.CASCADE,
     )
     date = models.DateField()
-    start_time = models.TimeField()
-    end_time = models.TimeField()
+    start_time = models.TimeField(blank=True)
+    end_time = models.TimeField(blank=True)
 
     class Meta:
         unique_together = [
@@ -50,7 +50,9 @@ class WorkoutLog(models.Model):
         return self(self.date)
 
 class Workout(models.Model):
-    workout_id = models.AutoField(primary_key=True)
+    workout_id = models.AutoField(
+        primary_key=True, editable=False
+    )
     exercise_id = models.ForeignKey(
         'Exercise',
         on_delete=models.CASCADE
@@ -63,9 +65,11 @@ class Workout(models.Model):
         return str(self.workout_id)
 
 class Exercise(models.Model):
-    exercise_id = models.AutoField(primary_key=True)
+    exercise_id = models.AutoField(
+        primary_key=True, editable=False
+    )
     name = models.CharField(max_length=35)
-    description = models.TextField()
+    description = models.TextField(blank=True)
 
     def __str__(self):
         return self.name
@@ -103,7 +107,9 @@ class WorkoutTemplate(models.Model):
         return self.name
 
 class WorkoutTemplateExercise(models.Model):
-    workout_exercises_id = models.AutoField(primary_key=True)
+    workout_exercises_id = models.AutoField(
+        primary_key=True, editable=False
+    )
     exercise_id = models.ForeignKey(
         'Exercise',
         on_delete=models.CASCADE
