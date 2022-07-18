05bd09da697e55edc2d1800857f7d274b360f8e4
jasonjdominguez99 <jasonjdominguez99@users.noreply.github.com>
2022-07-18 12:39:19 +0100

    Created admin classes and registered models

--- api/workout_tracker/admin.py
+++ api/workout_tracker/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import User
+from .models import (
+    User, UserWorkoutTemplate, Workout, WorkoutLog,
+    WorkoutTemplate, WorkoutTemplateExercise, Exercise
+)
 
 class UserAdmin(admin.ModelAdmin):
     list_display = (
@@ -11,4 +14,60 @@ class UserAdmin(admin.ModelAdmin):
         "sex"
     )
 
+class WorkoutLogAdmin(admin.ModelAdmin):
+    list_display = (
+        'user_id',
+        'workout_template_id',
+        'workout_id',
+        'date',
+        'start_time',
+        'end_time'
+    )
+
+class WorkoutAdmin(admin.ModelAdmin):
+    list_display = (
+        'workout_id',
+        'exercise_id',
+        'set_number',
+        'weight',
+        'reps'
+    )
+
+class ExerciseAdmin(admin.ModelAdmin):
+    list_display = (
+        'exercise_id',
+        'name',
+        'description'
+    )
+
+class UserWorkoutTemplateAdmin(admin.ModelAdmin):
+    list_display = (
+        'workout_template_id',
+        'user_id'
+    )
+
+class WorkoutTemplateAdmin(admin.ModelAdmin):
+    list_display = (
+        'workout_template_id',
+        'name',
+        'workout_exercises_id'
+    )
+
+class WorkoutTemplateExerciseAdmin(admin.ModelAdmin):
+    list_display = (
+        'workout_exercises_id',
+        'exercise_id'
+    )
+
 admin.site.register(User, UserAdmin)
+admin.site.register(WorkoutLog, WorkoutLogAdmin)
+admin.site.register(Workout, WorkoutAdmin)
+admin.site.register(Exercise, ExerciseAdmin)
+admin.site.register(
+    UserWorkoutTemplate, UserWorkoutTemplateAdmin
+)
+admin.site.register(WorkoutTemplate, WorkoutAdmin)
+admin.site.register(
+    WorkoutTemplateExercise,
+    WorkoutTemplateExerciseAdmin
+)
